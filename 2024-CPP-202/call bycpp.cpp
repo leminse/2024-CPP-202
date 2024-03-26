75cd05a679c9849f251b176b0f96ac52134c6f7e
leminse <s2330@e-mirim.hs.kr>
2024-03-26 15:41:32 +0900

    call_by_Pointer

--- 2024-CPP-202/call bycpp.cpp
+++ 2024-CPP-202/call bycpp.cpp
@@ -3,15 +3,16 @@
 //이름 공간. std::를 안해도 된다
 using namespace std;
 
-void call_by_value(int val) {
+void call_by_Pointer(int* val) {
 	
-	//새로운 지역 변수 val을 만들고 값을 넣는다.
-	val = 10;
+	//새로운 지역 변수 val을 만들고 val가 가리키고 있는 대상의 값을 바꾼다.
+	*val = 10;
 }
 
 int main(void) {
 	int num = 4;
-	call_by_value(num);
+	//num의 값은 바뀐다.
+	call_by_Pointer(&num);
 	cout << num;
 	return 0;
 }
