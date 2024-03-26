d9ca44aa442e7582bc65c0da7cd8393bdfbe5f56
leminse <s2330@e-mirim.hs.kr>
2024-03-26 16:03:31 +0900

    배열의 주소값

---
+++ 2024-CPP-202/배열과 포인트.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int arr[4] = { 10,20,30,40 };
+
+	char str[4] = "abc";						//4개면 3개 적기
+
+	//주솟값 4바이트 만큼의 차이
+	printf("%d %d\n", &arr[0],&arr[1]);			//ctrl + k + ctrl + c  주석
+	//주솟값 1바이트 만큼의 차이
+	printf("%d %d\n", &str[0],&str[1]);
+
+	return 0;
+}
