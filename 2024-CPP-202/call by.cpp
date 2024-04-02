ce14750aac3b67081b7b0842b16b688f62657ac6
leminse <s2330@e-mirim.hs.kr>
2024-04-02 15:49:37 +0900

    call by reference

--- 2024-CPP-202/call by.cpp
+++ 2024-CPP-202/call by.cpp
@@ -3,16 +3,16 @@
 //이름 공간. std::를 안해도 된다
 using namespace std;
 
-void call_by_Pointer(int* val) {
+void call_by_reference(int& val) {
 	
 	//새로운 지역 변수 val을 만들고 val가 가리키고 있는 대상의 값을 바꾼다.
-	*val = 10;
+	val = 10;
 }
 
 int main(void) {
 	int num = 4;
 	//num의 값은 바뀐다.
-	call_by_Pointer(&num);
+	call_by_reference(num);
 	cout << num;
 	return 0;
 }
