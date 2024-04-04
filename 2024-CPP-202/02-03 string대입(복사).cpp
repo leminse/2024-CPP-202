5aa29727478e99d99cd2f6113bef3c449c93e052
leminse <s2330@e-mirim.hs.kr>
2024-04-04 11:42:33 +0900

    String 대입2 (C언어 방식2)

--- 2024-CPP-202/02-03 string대입(복사).cpp
+++ 2024-CPP-202/02-03 string대입(복사).cpp
@@ -1,16 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 	char str[30] = "develop hungry";
 	char str2[30];
 
 	//str에 있는 내용을 str2에 복사
-	//'\0'을 만날때 까지
-	int i = 0;
-	for (; str[i] != '\0'; i++) 				//for문은 한 줄 일시 {}사용 안해도 됨.
-		str2[i] = str[i];
-
-	str2[i] = str[i];							//널문자('\0')복사
+	strcpy(str2, str);
 
 	printf("%s", str2);
 
