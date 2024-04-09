b381efbb337e8257bed6eee97fadb3c9bbf35138
leminse <s2330@e-mirim.hs.kr>
2024-04-09 15:42:59 +0900

    클래스는 default가 private이다.

--- 2024-CPP-202/03-01 구조체와 클래스.cpp
+++ 2024-CPP-202/03-01 구조체와 클래스.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-struct Student {
+class Student {
 	int hakbun_;			//구글방식 (변수명_)   _을 붙임
 	string name_;
 };
 
 int main(void) {
-	struct Student jisu;
-	//구조체는 default(자동값)가 public이다.
+	Student jisu;								//struct Student 안해도 됨.
+	//class는 default(자동값)가 private이다.
 	jisu.hakbun_ = 2202;
 	jisu.name_ = "나지수";
 
