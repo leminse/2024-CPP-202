6288d1b6bfb72f25307551d97e888f5d17025d63
leminse <s2330@e-mirim.hs.kr>
2024-04-09 15:04:29 +0900

    string 문자열 길이(C++방식)

---
+++ 2024-CPP-202/02-06 string 문자열 길이.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(void) {			//글자길이(숫자로 알려줌)
+	string str = "c++";
+	cout << str.size() << endl;											//Ctrl + D 다음줄 붙여넣기	
+	cout << str.length() << endl;
+
+
+	
+
+}
