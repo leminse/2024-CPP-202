25afaa68cef009aee41ed86e993883c791809957
leminse <s2330@e-mirim.hs.kr>
2024-04-18 11:44:05 +0900

    03-02 생성자, 소멸자 호출시기 확인

--- 2024-CPP-202/03-02 생성자와 소멸자.cpp
+++ 2024-CPP-202/03-02 생성자와 소멸자.cpp
@@ -12,23 +12,24 @@ public:
 
 		//'\'가 들어가기 위해 공간 하나 더 추가
 		name_ = new char[length + 1];
-
-		strcpy(name_, name);							//사용하지 않을 시 이상한 한자나옴
+		strcpy(name_, name);							
+		cout << "생성자 호출 완료" << endl;
 	}
 
-	//소멸자 : 소멸될 때 (메모리 사라질 때) 호출되는 함수
+	//소멸자 : 소멸될 때 (메모리에서 지워질 때) 호출되는 함수
 	~Student(void) {
-		delete[] name_;								//배열 여러개 만들었으므로 []붙이기
+		delete []name_;												//배열 여러개 만들었으므로 []붙이기
+		cout << "소멸자 호출 완료" << endl;
 	}
 	
 	//클래스의 멤버를 출력
-	void show(void) {			//void는 메개변수
+	void show(void) {												//void는 메개변수
 		cout << hakbun_ << " ";
 		cout << name_ << endl;
 	}
 
 private:
-	int hakbun_;			//구글방식 (변수명_)   _을 붙임
+	int hakbun_;													//구글방식 (변수명_)   _을 붙임
 	char* name_;
 
 };
@@ -36,9 +37,10 @@ private:
 int main(void) {
 	//동적할당 : 메모리는 heap에서 할당.	실행(런타임) 시 메모리 크기가 정해짐. heap는 많은 메모리를 만들 수 있으며, 많은 메모리 관리 가능
 	Student* stu = new Student(2208,"이민서");
-//	stu.show();
-	stu->show();
-	delete stu;											//delete를 사용 시 객체 2208만 지워지고 "이민서"는 지워지지 않음
+	cout << "I'm still hungry - 거스 히딩크" << endl;
+
+	delete stu;											
+	cout << "It ain't over itll it's over - 요기 베라" << endl;
 	
 
 	return 0;
