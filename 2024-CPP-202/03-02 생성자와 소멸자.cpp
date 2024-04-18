3127779a04a169effd261d5e56687bdf5a5e36f2
leminse <s2330@e-mirim.hs.kr>
2024-04-18 11:14:46 +0900

    03-02 소멸자

--- 2024-CPP-202/03-02 생성자와 소멸자.cpp
+++ 2024-CPP-202/03-02 생성자와 소멸자.cpp
@@ -7,14 +7,18 @@ using namespace std;
 class Student {
 public:		
 
-	//TODO : 생성자에서 동적할당을 한 메모리 공간을 지울 수 없다.					//todo 한국 뜻 할 일
 	Student(int hakbun, const char* name) : hakbun_(hakbun) {		
 		int length = strlen(name);
 
 		//'\'가 들어가기 위해 공간 하나 더 추가
-		name_ = new char[length+1];
+		name_ = new char[length + 1];
 
 		strcpy(name_, name);							//사용하지 않을 시 이상한 한자나옴
+	}
+
+	//소멸자 : 소멸될 때 (메모리 사라질 때) 호출되는 함수
+	~Student(void) {
+		delete[] name_;								//배열 여러개 만들었으므로 []붙이기
 	}
 	
 	//클래스의 멤버를 출력
@@ -34,9 +38,9 @@ int main(void) {
 	Student* stu = new Student(2208,"이민서");
 //	stu.show();
 	stu->show();
-	delete stu;											//delete를 사용 시 객체 2208만 지워지고 객체 "이민서"는 지워지지 않음
+	delete stu;											//delete를 사용 시 객체 2208만 지워지고 "이민서"는 지워지지 않음
 	
 
 	return 0;
 
-}
+} 
