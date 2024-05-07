dc301460a3f0e6d57be823ec642e4317114e5806
leminse <s2330@e-mirim.hs.kr>
2024-05-07 16:05:04 +0900

    03-03 깊은복사

--- 2024-CPP-202/03-03 복사 생성자.cpp
+++ 2024-CPP-202/03-03 복사 생성자.cpp
@@ -16,10 +16,22 @@ public:
 		cout << "생성자 호출 완료" << endl;
 	}
 
+	//복사생성자
+	/*Student(const Student& rhs) {
+		cout << "복사생성자 호출 완료" << endl;
+	}*/
 
 	//복사생성자(얕은 복사)
-	Student(const Student& rhs) :hakbun_(rhs.hakbun_), name_(rhs.name_) {				
+	Student(const Student& rhs) :hakbun_(rhs.hakbun_), name_(rhs.name_) {
 		cout << "복사생성자 호출 완료" << endl;
+
+		//깊은 복사
+		int length = strlen(rhs.name_);
+
+		//'\'가 들어가기 위해 공간 하나 더 추가
+		name_ = new char[length + 1];
+		strcpy(name_, rhs.name_);
+		cout << "생성자 호출 완료" << endl;
 	}
 
 	//소멸자 : 소멸될 때 (메모리에서 지워질 때) 호출되는 함수, 소멸자 선언 시 ~사용
@@ -47,8 +59,6 @@ int main(void) {
 
 	//복사 생성자
 	Student stu2 = stu;
-
-	//TODO : 얕은 복사로 인하여 같은 주소(stu.name_, stu2.name_)에서 두 번 delete 시도
 
 	return 0;
 
