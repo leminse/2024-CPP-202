0aebea7d8eb98b0ad605af0a76349c8e6aaf5424
leminse <s2330@e-mirim.hs.kr>
2024-05-28 16:05:48 +0900

    05-01 보모/자식 생성자/소멸자 호출 순서

--- 2024-CPP-202/05-01 상속기본.cpp
+++ 2024-CPP-202/05-01 상속기본.cpp
@@ -5,7 +5,12 @@ using namespace std;
 class Person {
 public:
 	Person(string name, int age)
-		:name_(name), age_(age) {}
+		:name_(name), age_(age) {
+		cout << "부모 생성자 호출" << endl;
+	}
+	~Person() {
+		cout << "부모 소멸자 호출" << endl;
+	}
 
 	void show_person() {
 		cout << name_ << "의 나이는" << age_ << "입니다." << endl;
@@ -21,7 +26,10 @@ public:
 	//부모 생성자가 먼저 호출
 	Student(string name, int age, int id) :Person(name, age), id_(id)
 	{
-
+		cout << "자식 생성자 호출" << endl;
+	}
+	~Student() {
+		cout << "자식 소멸자 호출" << endl;
 	}
 
 	void ShowPerson() {		//override 오바라이드다.
@@ -37,6 +45,7 @@ int main() {
 	//동적할당으로 만들기
 	Student* stu = new Student("다람쥐", 1212, 12345);
 	stu->ShowPerson();
+	delete stu;
 
 	return 0;
 }
