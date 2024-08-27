ef8547b935b46220d39e255960dc08221e29c6b0
leminse <s2330@e-mirim.hs.kr>
2024-08-27 18:23:27 +0900

    순수 가상함수

--- 2024-CPP-202/05-03 다형성.cpp
+++ 2024-CPP-202/05-03 다형성.cpp
@@ -8,8 +8,6 @@ public:
     Animal(string name, unsigned int age, int leg_num)
         : name_(name), age(age), leg_num_(leg_num) {}
 
-
-
     virtual void walk(void) {  // virtual 키워드 추가
         cout << "걷다" << endl;
     }
@@ -25,6 +23,10 @@ public:
     void cute(void) {
         cout << "매우 귀엽다" << endl;
     }
+
+    // 순수 가상함수(추상메서드)
+    virtual void sleep(void) = 0;  // 순수 가상 함수
+    virtual void makeSound(void) = 0;  // 순수 가상 함수
 
 private:
     string name_;
@@ -52,15 +54,26 @@ public:
         cout << "조용조용" << endl;
     }
 
+    void sleep() override {  // 순수 가상 함수 구현
+        cout << "Zzz..." << endl;
+    }
+
+    void makeSound() override {  // 순수 가상 함수 구현
+        cout << "고양이 소리" << endl;
+    }
+
 private:
     int cute_level;
 };
 
-int main(void) { 
+int main(void) {
 
+    //추상클래스는 객체를 생성할 수 없다(new Animal() 불가)
     Animal* animal = new Cat("루나", 6, 4, 100);
- 
+    animal->bark();
+    animal->makeSound();
+    animal->sleep();
     delete animal;
 
-    return 0; 
+    return 0;
 }
