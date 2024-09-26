76d33f6fb05f9bb03b4e1cc54372bc96ea71c861
leminse <s2330@e-mirim.hs.kr>
2024-09-26 11:12:29 +0900

    09-01 enum

--- 2024-CPP-202/08-02 맵.cpp
+++ 2024-CPP-202/08-02 맵.cpp
@@ -1,28 +1,43 @@
-﻿﻿#include <iostream>
+﻿#include <iostream>
 #include <map>
+#include <vector>
 
 using namespace std;
 
-void main(void)
+int main() // main의 반환 타입을 int로 수정
 {
-	map<string, long long> money;
+    map<string, long long> v;
 
-	// 맵 추가
-	money["하츄핑"] = 10000000000;
-	money["샤샤핑"] = 30000000000;
-	money["공쥬핑"] = 100;
+    // 맵 추가
+    v["하츄핑"] = 10000000000;
+    v["샤샤핑"] = 30000000000;
+    v["공쥬핑"] = 100;
 
+    // 반복자
+    for (auto iter = v.begin(); iter != v.end(); iter++)
+        cout << iter->first << "는 " << iter->second << endl;
 
-	// 반복자
-	map<string, long long>::iterator iter;
-	for (iter = money.begin(); iter != money.end(); iter++)
-		cout << iter->first << "는 " << iter->second << endl;
+    // 값 수정
+    v["하츄핑"] = 10000000000;
+    cout << v["하츄핑"] << endl;
 
-	// 값 수정
-	money["하츄핑"] = 10000000000;
-	cout << money["하츄핑"] << endl;
+    // 크기
+    cout << "맵의 크기: " << v.size() << endl;
 
-	// 크기
-	cout << money.size() << endl;
+    // 값 출력
+    for (auto iter = v.begin(); iter != v.end(); iter++)
+        cout << iter->second << endl;
+     
+    // 끝의 값 제거 (마지막 항목 삭제)
+    if (!v.empty()) {
+        auto last = --v.end(); // 마지막 요소의 반복자
+        v.erase(last); // 마지막 요소 삭제
+    }
 
+    // 남은 값 출력
+    cout << "남은 값들:" << endl;
+    for (auto iter = v.begin(); iter != v.end(); iter++)
+        cout << iter->first << "는 " << iter->second << endl;
+
+    return 0; // 성공적으로 종료
 }
---
+++ 2024-CPP-202/09-01 enum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+//열거형 정의
+enum FoodStyle {
+	KOREAN,							//0	(0은 디폴트 값이고 값을 주고 싶으면 KOREAN=0으로 한다)
+	JAPANESE,						//1
+	CHINESE							//2	
+};
+
+void main(void) {
+	
+	//열거형을 사용하면 가독성도 좋아지고, 중간에 값을 유연하게 추가할 수 있다.
+
+	int style = KOREAN;
+
+	switch (style) {
+		case KOREAN:
+			cout << "김치, 쌈, 비빔밥" << endl;
+			break;
+		case JAPANESE:
+			cout << "초밥, 타코야끼, 라멘" << endl;
+			break;
+		case CHINESE:
+			cout << "마라탕, 탕후루, 동파육" << endl;
+			break;
+	}
+	
+}
