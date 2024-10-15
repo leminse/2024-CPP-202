aac54d8ebd26e3bd2dc39aa623559558b74e300e
이민서 <sonichoko10@gmail.com>
2024-10-15 14:27:52 +0900

    09-02 범위기반 for문

---
+++ 2024-CPP-202/09-02 범위기반 for문.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main(void) {
+	vector<int> v = { 445, 14, 27, 456, 23 };
+
+	for (int value : v) {
+		cout << value << endl;
+	}
+}
