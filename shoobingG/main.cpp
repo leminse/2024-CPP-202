070259a6c72201e630212f9ad552dbbed6f89b6d
이민서 <sonichoko10@gmail.com>
2024-10-17 12:11:33 +0900

    [shoobingG] 플레이어 띄우기

---
+++ shoobingG/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+void main(void) {
+	RenderWindow window(VideoMode(1200, 700), "shootingG");
+
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		RectangleShape player;
+		player.setSize(Vector2f(181.f, 73.f));
+		player.setFillColor(Color::Green);
+		player.setPosition(500.f, 400.f);
+
+
+		window.clear();
+		window.draw(player);
+		window.display();
+	}
+}
