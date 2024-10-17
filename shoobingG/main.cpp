a3ff099bcbf93bb0c0046ec7d057d9155389c545
이민서 <sonichoko10@gmail.com>
2024-10-17 12:12:02 +0900

    [shoobingG] 상수 처리 방법 1

--- shoobingG/main.cpp
+++ shoobingG/main.cpp
@@ -3,6 +3,9 @@
 using namespace sf;
 using namespace std;
 
+#define WINDOW_WIDTH  1200;
+#define WINDOW_HEIGHT  700;
+
 void main(void) {
 	RenderWindow window(VideoMode(1200, 700), "shootingG");
 
