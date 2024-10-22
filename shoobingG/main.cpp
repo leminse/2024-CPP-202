e0e8397d72c40c2f352d0128fa53d1a38bad8456
이민서 <sonichoko10@gmail.com>
2024-10-22 14:50:24 +0900

    [shoobingG] 모든 컴퓨터 사양에 맞게 프레임 조절

--- shoobingG/main.cpp
+++ shoobingG/main.cpp
@@ -12,34 +12,34 @@ enum App {
 void main(void)
 {
 	RenderWindow window(VideoMode(App::WIDTH, App::HEIGHT), "shootingG");
+	window.setFramerateLimit(60);
+	RectangleShape player;
+	player.setSize(Vector2f(181.f, 75.f));
+	player.setFillColor(Color::Magenta);
+	player.setPosition((App::WIDTH - player.getSize().x) / 2.f, 600.f);
 
 	while (window.isOpen()) {
 		Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		RectangleShape player;
-		player.setSize(Vector2f(181.f, 75.f));
-		player.setFillColor(Color::Magenta);
-		player.setPosition((App::WIDTH - player.getSize().x) / 2.f, 600.f);
-
-		// TODO : 제대로 이동하도록 고치기
-		if (Keyboard::isKeyPressed(Keyboard::Left))
-			player.move(-10, 0);
-		if (Keyboard::isKeyPressed(Keyboard::Right))
-			player.move(10, 0);
-		if (Keyboard::isKeyPressed(Keyboard::Up))
-			player.move(0, -10);
-		if (Keyboard::isKeyPressed(Keyboard::Down))
-			player.move(0, 10);
-
-
-		window.clear();
-		window.draw(player);
-		window.display();
-
+			window.close();
 	}
+
+	RectangleShape player;
+	player.setSize(Vector2f(181.f, 75.f));
+	player.setFillColor(Color::Magenta);
+	player.setPosition((App::WIDTH - player.getSize().x) / 2.f, 600.f);
+	// TODO : 제대로 이동하도록 고치기
+	if (Keyboard::isKeyPressed(Keyboard::Left))
+		player.move(-10, 0);
+	if (Keyboard::isKeyPressed(Keyboard::Right))
+		player.move(10, 0);
+	if (Keyboard::isKeyPressed(Keyboard::Up))
+		player.move(0, -10);
+	if (Keyboard::isKeyPressed(Keyboard::Down))
+		player.move(0, 10);
+
+
+	window.clear();
+	window.draw(player);
+	window.display();
+
 }
